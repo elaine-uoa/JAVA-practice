dd5172d5ad622160f90b1fc1fb4aaf93ece7dbc9
elaine-uoa <byhz081@aucklanduni.ac.nz>
2026-02-20 14:36:43 +1300

    Add some templates for now

---
+++ week-01/Day1/src/Day1Exercises.java
@@ -0,0 +1,38 @@
+public class Day1Exercises {
+
+    public static void main(String[] args) {
+        System.out.println("Day 1 start.");
+
+        // Quick self-tests
+        System.out.println("isEven(4) = " + isEven(4));   // true
+        System.out.println("sumToN(5) = " + sumToN(5));   // 15
+        System.out.println("isPrime(7) = " + isPrime(7)); // true
+        System.out.println("factorial(5) = " + factorial(5)); // 120
+        System.out.println("reverse(\"abc\") = " + reverse("abc")); // "cba"
+    }
+
+    public static boolean isEven(int n) {
+        // TODO
+        return false;
+    }
+
+    public static int sumToN(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static boolean isPrime(int n) {
+        // TODO
+        return false;
+    }
+
+    public static long factorial(int n) {
+        // TODO
+        return 0L;
+    }
+
+    public static String reverse(String s) {
+        // TODO
+        return "";
+    }
+}

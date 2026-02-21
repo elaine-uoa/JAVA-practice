d9be0553ad2bc0aef7231b61c018854e10db0d1a
elaine-uoa <byhz081@aucklanduni.ac.nz>
2026-02-21 22:13:04 +1300

    Add Day1Exercises random coin flip example

--- week-01/Day1/src/Day1Exercises.java
+++ week-01/Day1/src/Day1Exercises.java
@@ -1,38 +1,19 @@
+import java.util.Random;
+
 public class Day1Exercises {
 
     public static void main(String[] args) {
-        System.out.println("Day 1 start.");
 
-        // Quick self-tests
-        System.out.println("isEven(4) = " + isEven(4));   // true
-        System.out.println("sumToN(5) = " + sumToN(5));   // 15
-        System.out.println("isPrime(7) = " + isPrime(7)); // true
-        System.out.println("factorial(5) = " + factorial(5)); // 120
-        System.out.println("reverse(\"abc\") = " + reverse("abc")); // "cba"
-    }
+        Random random = new Random();
+        boolean isHeads;
 
-    public static boolean isEven(int n) {
-        // TODO
-        return false;
-    }
+        isHeads = random.nextBoolean();
 
-    public static int sumToN(int n) {
-        // TODO
-        return 0;
-    }
+        if (isHeads) {
+            System.out.println("It's a head!");
+        }  else {
+            System.out.println("It's a tail!");
+        }
 
-    public static boolean isPrime(int n) {
-        // TODO
-        return false;
-    }
-
-    public static long factorial(int n) {
-        // TODO
-        return 0L;
-    }
-
-    public static String reverse(String s) {
-        // TODO
-        return "";
     }
 }

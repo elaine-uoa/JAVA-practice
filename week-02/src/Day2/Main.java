3284a49d095ca3a6ad863f7775272ff0fa3f86ab
elaine-uoa <byhz081@aucklanduni.ac.nz>
2026-03-04 10:37:01 +1300

    Week 2: Day 2 practice, about constructors.

---
+++ week-02/src/Day2/Main.java
@@ -0,0 +1,26 @@
+package Day2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Spngebob", 30, 3.2);
+        Student student2 = new Student("Patric", 34, 1.5);
+
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
+
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
+
+
+
+
+
+    }
+
+}
---
+++ week-02/src/Day2/Student.java
@@ -0,0 +1,17 @@
+package Day2;
+
+public class Student {
+
+    String name;
+    int age;
+    double gpa;
+    boolean isEnrolled;
+
+    Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+        this.isEnrolled = true;
+    }
+
+}

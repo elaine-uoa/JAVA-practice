39b03acdad888925c6b8a82b9ea3b0b8722be269
elaine-uoa <byhz081@aucklanduni.ac.nz>
2026-03-19 21:24:31 +1300

    Week-04 practice: Day4, a music player.

---
+++ week-04/src/Day4_MusicPlayer/Main.java
@@ -0,0 +1,62 @@
+package Day4_MusicPlayer;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filePath = "/Users/elaine/Desktop/JAVA-practice/week-04/src/Day4_MusicPlayer/twisterion-twisterion-b1-221376.wav";
+
+        File file = new File(filePath);
+
+        try(Scanner scanner = new Scanner(System.in);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)){
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+
+            String response = "";
+
+            while(!response.equals("Q")) {
+
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+                System.out.print("Enter your choice: ");
+
+                response = scanner.next().toUpperCase();
+
+                switch (response) {
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Could not locate file");
+        }
+        catch (UnsupportedAudioFileException e){
+            System.out.println("Audio file is not supported");
+        }
+        catch (LineUnavailableException e) {
+            System.out.println("Unable to access audio resource");
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong");
+        }
+        finally {
+            System.out.println("Bye!");
+        }
+
+    }
+}
